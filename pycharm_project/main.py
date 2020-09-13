39e62b208e679ebdb3f8c9c7405d525b4ae82719
Adrian Schwizgebel <adrian.schwizgebel@gmail.com>
2020-09-13 21:30:14 +0200

    Added some functions
    psutil has limited functionality in windows. need to search for another module for temp and fan sensors.

--- pycharm_project/main.py
+++ pycharm_project/main.py
@@ -1,11 +1,11 @@
 #
 from __future__ import print_function
 import psutil
-print(psutil.cpu_percent())
-print(psutil.virtual_memory())
-print('memory % used:', psutil.virtual_memory()[2])
-print(psutil.cpu_count())
-print(psutil.cpu_freq())
-print(psutil.cpu_stats())
-print(psutil.disk_usage('C:/'))
-print(psutil.disk_usage('E:/'))
+import sys
+
+print('cpu_percent:\t', psutil.cpu_percent(interval=1), '%')
+print('memory_used:\t', psutil.virtual_memory()[2], '%')
+print('cpu_count:\t\t', psutil.cpu_count(logical=False), 'cores')
+print('cpu_freq:\t\t', psutil.cpu_freq())
+print('disk_usage C:\t', psutil.disk_usage('C:/')[3], '%')
+print('disk_usage D:\t', psutil.disk_usage('D:/')[3], '%')
